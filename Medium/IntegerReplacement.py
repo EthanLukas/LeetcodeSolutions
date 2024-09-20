484f9991f9c623024cff60032d8843459ca4f8d1
Ethan Lukas <ethanalukas@gmail.com>
2024-09-20 12:33:13 -0700

    Memoization

--- Medium/IntegerReplacement.py
+++ Medium/IntegerReplacement.py
@@ -1,28 +1,34 @@
 class Solution:
+
+    def __init__(self):
+        self.memo = dict()
         
     def integerReplacement(self, n: int) -> int:
+
+        if n in self.memo:
+            return self.memo[n]
         
         if n == 1:
+            self.memo[1] = 0
             return 0
         elif n == 2:
+            self.memo[2] = 1
             return 1
         elif n == 3:
+            self.memo[3] = 2
             return 2
 
         # Even
         if n % 2 == 0:
-            # if (n//2) in memo:
-            #     return 1 + memo(n//1)
-            # else:
-                # memo[n//2] = self.integerReplacement(n//2)
-                return 1 + self.integerReplacement(n//2)
+            valRet = 1 + self.integerReplacement(n//2)
+            self.memo[n] = valRet
+            return valRet
 
         # Odd
         else:
-            # if (n+1) in memo:
-            #     val1 = memo(n//1)
-            #     val2 = memo(n//1)
-            return 1 + min(self.integerReplacement(n+1), self.integerReplacement(n-1))
+            valRet = 1 + min(self.integerReplacement(n+1), self.integerReplacement(n-1))
+            self.memo[n] = valRet
+            return valRet
 
 
         
